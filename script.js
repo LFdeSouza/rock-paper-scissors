7f11d9be14c97a0575671579b71de6bd324a29db
LFS1245 <luiz.f.souza@live.com>
2021-08-22 17:34:28 -0300

    game old version incomplete

--- script.js
+++ script.js
@@ -1,68 +1,103 @@
-//Create a function that returns computer selection
-function computerSelection() {
-  const random = Math.random() * 3;
-  if (random < 1) {
+// get all the items from the page
+const possibleChoices = document.querySelectorAll(".game-button");
+const playerChoiceDisplay = document.querySelector("#playerChoiceDisplay");
+const computerChoiceDisplay = document.querySelector("#computerChoiceDisplay");
+const scorePlayer = document.querySelector("#scorePlayer");
+const scoreTies = document.querySelector("#scoreTies");
+const scoreComputer = document.querySelector("#scoreComputer");
+const winnerBoard = document.querySelector(".winner-board");
+let userChoice = null;
+let wins = 0;
+let loses = 0;
+let ties = 0;
+
+//get input from user, break
+possibleChoices.forEach((choice) =>
+  choice.addEventListener("click", () => {
+    userChoice = choice.id;
+    let computerChoice = getComputerChoice();
+    uptdateDisplays(userChoice, computerChoice);
+    let result = playRound(userChoice, computerChoice);
+    updateScores(result);
+  })
+);
+
+//play round
+function playRound(userChoice, computerChoice) {
+  if (userChoice === computerChoice) {
+    return "tie";
+  } else if (userChoice === "rock" && computerChoice === "scissors") {
+    uptdateDisplays(userChoice, computerChoice);
+    return "win";
+  } else if (userChoice === "rock" && computerChoice === "paper") {
+    uptdateDisplays(userChoice, computerChoice);
+    return "lose";
+  } else if (userChoice === "paper" && computerChoice === "rock") {
+    uptdateDisplays(userChoice, computerChoice);
+    return "win";
+  } else if (userChoice === "paper" && computerChoice === "scissors") {
+    uptdateDisplays(userChoice, computerChoice);
+    return "lose";
+  } else if (userChoice === "scissors" && computerChoice === "paper") {
+    uptdateDisplays(userChoice, computerChoice);
+    return "win";
+  } else if (userChoice === "scissors" && computerChoice === "rock") {
+    uptdateDisplays(userChoice, computerChoice);
+    return "lose";
+  }
+}
+
+// get random number for computer choice
+function getComputerChoice() {
+  const computerChoice = Math.random() * 3;
+  if (computerChoice < 1) {
     return "rock";
-  } else if (random < 2) {
+  } else if (computerChoice < 2) {
     return "paper";
   } else {
     return "scissors";
   }
 }
-//function that takes player selection (use prompt)
-function playerSelection() {
-  const userChoice = window.prompt("Please enter your choice: ").toLowerCase();
 
-  return userChoice;
-}
+//update displays
+function uptdateDisplays(userChoice, computerChoice) {
+  if (userChoice === "rock") {
+    playerChoiceDisplay.innerHTML =
+      '<img src="https://img.icons8.com/ios/100/000000/hand-rock.png"/>';
+  } else if (userChoice === "paper") {
+    playerChoiceDisplay.innerHTML =
+      '<img src="https://img.icons8.com/ios/100/000000/hand.png" />';
+  } else if (userChoice === "scissors") {
+    playerChoiceDisplay.innerHTML =
+      '<img src="https://img.icons8.com/ios/100/000000/hand-scissors--v1.png" />';
+  }
 
-//function plays a single round
-function playRound(userChoice, computerChoice) {
-  if (userChoice === computerChoice) {
-    return "tie";
-  } else if (userChoice === "rock" && computerChoice === "scissors") {
-    return "win";
-  } else if (userChoice === "rock" && computerChoice === "paper") {
-    return "lose";
-  } else if (userChoice === "paper" && computerChoice === "rock") {
-    return "win";
-  } else if (userChoice === "paper" && computerChoice === "scissors") {
-    return "lose";
-  } else if (userChoice === "scissors" && computerChoice === "paper") {
-    return "win";
-  } else if (userChoice === "scissors" && computerChoice === "rock") {
-    return "lose";
+  if (computerChoice === "rock") {
+    computerChoiceDisplay.innerHTML =
+      '<img src="https://img.icons8.com/ios/100/000000/hand-rock.png"/>';
+  } else if (computerChoice === "paper") {
+    computerChoiceDisplay.innerHTML =
+      '<img src="https://img.icons8.com/ios/100/000000/hand.png" />';
+  } else if (computerChoice === "scissors") {
+    computerChoiceDisplay.innerHTML =
+      '<img src="https://img.icons8.com/ios/100/000000/hand-scissors--v1.png" />';
   }
 }
 
-function game() {
-  let wins = 0,
-    loses = 0,
-    ties = 0;
-  counter = 0;
-  while (counter < 5) {
-    counter += 1;
-    const computerChoice = computerSelection();
-    const userChoice = playerSelection();
-    const result = playRound(userChoice, computerChoice);
-    if (result === "win") {
-      wins += 1;
-      console.log("You win!");
-    } else if (result === "lose") {
-      loses += 1;
-      console.log("You lose!");
-    } else if (result === "tie") {
-      ties += 1;
-      console.log("Tie!");
-    }
+function updateScores(result) {
+  if (result === "tie") {
+    ties += 1;
+    scoreTies.innerHTML = ties;
+    winnerBoard.innerHTML = "TIE!";
   }
-  if (wins > loses) {
-    console.log("Player wins the game!");
-  } else if (loses > wins) {
-    console.log("Computer wins the game!");
-  } else {
-    console.log("It's a Tie!");
+  if (result === "lose") {
+    loses += 1;
+    scoreComputer.innerHTML = loses;
+    winnerBoard.innerHTML = "COMPUTER WINS!";
+  }
+  if (result === "win") {
+    wins += 1;
+    scorePlayer.innerHTML = wins;
+    winnerBoard.innerHTML = "YOU WIN!";
   }
 }
-game();
-//function game that plays 5 rounds
